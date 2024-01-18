400f73c65372ce28198594bf5689248ae33cac0f
Alan G. Sanchez <sanchala@oregonstate.edu>
2024-01-18 15:10:32 -0800

    Included comments

--- tb3_ros2_tutorials/move.py
+++ tb3_ros2_tutorials/move.py
@@ -10,68 +10,86 @@ from geometry_msgs.msg import Twist
 
 
 class Move(Node):
-	"""
-	A class that sends Twist messages to move the Turtlebot forward.
-	"""
+	'''
+	A class that sends Twist messages to move the Turtlebot Burger forward.
+	'''
 	def __init__(self):
-		"""
+		'''
 		A constructor that initializes the parent class and publisher.
-		@param self: The self reference
-		"""
-		## Initialize parent class. The idiom is to use the `super()` class and it calls the
+
+		Parameters:
+		- self: The self reference
+		'''
+		## Initialize parent class, giving it a name. The idiom is to use the `super()` class and it calls the
 		## `Node` class's constructor.
-		super().__init__('publisher')
+		super().__init__('twist_publisher')
 
 		## Create a publisher, and assign it to a member variable. The call takes a type, 
 		## topic name, and queue size.
-		self.pub = rospy.Publisher(Twist,'cmd_vel', queue_size=1) 
+		self.pub = self.create_publisher(Twist,'/cmd_vel', 10) 
+
+		## Rather than setting up a Rate-controller loop, the idiom in ROS2 is to use timers.
+		## Timers are available in the Node interface, and take a period (in seconds), and a
+		## callback.  Timers are repeating by default.
+		self.timer = self.create_timer(1, self.move_forward)
+
+		# Log that we published something.  In ROS2, loggers are associated with nodes, and
+		# the idiom is to use the get_logger() call to get the logger.  This has functions
+		# for each of the logging levels.
+		self.get_logger().info('The {0} class is up and running. Sending Twist commands to the Turtlebot.'.format(self.__class__.__name__))
 
 	def move_forward(self):
-		"""
+		'''
 		Function that publishes Twist messages
-		@param self: The self reference.
+		Parameters:
+		- self: The self reference
 
-		@publishes command: Twist message.
-		"""
+		Publisher
+		- command (Twist): base velocity commands for the Turtlebot.
+		'''
 		# Make a Twist message.  We're going to set all of the elements, since we
 		# can't depend on them defaulting to safe values
 		command = Twist()
 
 		# A Twist has three linear velocities (in meters per second), along each of the axes.
-		# For Stretch, it will only pay attention to the x velocity, since it can't
-		# directly move in the y direction or the z direction
-		command.linear.x = 0.1
+		# For the turtlebot, it will only pay attention to the x velocity, since it can't
+		# directly move in the y or z direction
+		command.linear.x = 0.0
 		command.linear.y = 0.0
 		command.linear.z = 0.0
 
 		# A Twist also has three rotational velocities (in radians per second).
-		# The Stretch will only respond to rotations around the z (vertical) axis
+		# The turtlebot will only respond to rotations around the z (vertical) axis
 		command.angular.x = 0.0
 		command.angular.y = 0.0
-		command.angular.z = 0.0
+		command.angular.z = 0.04
 
 		# Publish the Twist commands
 		self.pub.publish(command)
 
-if __name__ == '__main__':
-	# Initialize the node, and call it "move"
-	rospy.init_node('move')
+## The idiom in ROS2 is to use a function to do all of the setup and work.  This
+## function is referenced in the setup.py file as the entry point of the node when
+## we're running the node with ros2 run.  The function should have one argument, for
+## passing command line arguments, and it should default to None.
+def main(args=None):
+	## Initialize rclpy.  We should do this every time.
+	rclpy.init(args=args)
 
-	# Declare object from Move class
+	## Declare object from the `Move` class
 	base_motion = Move()
 
-	# Rate allows us to control the (approximate) rate at which we publish things.
-	# For this example, we want to publish at 10Hz
-	rate = rospy.Rate(10)
+	## The spin() call gives control over to ROS2, and it now takes a Node-derived
+	## class as a parameter.
+	rclpy.spin(base_motion)
 
-	# This will loop until ROS shuts down the node.  This can be done on the
-	# command line with a ctrl-C, or automatically from roslaunch
-	while not rospy.is_shutdown():
-		# Run the move_foward function in the Move class
-		base_motion.move_forward()
+	## Make sure we shutdown everything cleanly.  This should happen, even if we don't
+	## include this line, but you should do it anyway.
+	base_motion.destroy_node()
+	rclpy.shutdown()
 
-		# Do an idle wait to control the publish rate. If we don't control the
-		# rate, the node will publish as fast as it can, and consume all of the
-		# available CPU resources. This will also add a lot of network traffic,
-		# possibly slowing down other things
-		rate.sleep()
+## If we run the node as a script, then we're going to start here.
+if __name__ == '__main__':
+	## The idiom in ROS2 is to set up a main() function and to call it from the entry
+	## point of the script.
+	main()
+
